d9de8c5b95616e9c7f90f04c39cd33ca5c307813
Douaa2003 <Douaatbibi9@gmail.com>
2026-01-12 00:52:52 +0100

    Mise à jour du projet  nouvelle version

--- App.js
+++ App.js
@@ -3,12 +3,31 @@ import AuthProvider, { AuthContext } from "./context/AuthContext";
 import AppDrawer from "./navigation/AppDrawer";
 import LoginScreen from "./screens/LoginScreen";
 import { useContext } from "react";
+import { View, StyleSheet } from "react-native";
+import { ThemeProvider, ThemeContext } from "./context/ThemeContext";
+import TodoListFetchScreen from "./screens/TodoListFetchScreen";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
+
+
 function RootNavigator() {
 const { user } = useContext(AuthContext);
 return user ? <AppDrawer /> : <LoginScreen />;
 }
+
+function MainApp() {
+const { theme } = useContext(ThemeContext);
+return (
+<View
+style={[styles.container,
+theme === "dark" ? styles.dark : styles.light,
+]}
+>
+<TodoListFetchScreen />
+</View>
+);
+}
+
 export default function App() {
 return (
  <Provider store={store}>
@@ -16,7 +35,24 @@ return (
  <NavigationContainer>
  <RootNavigator />
  </NavigationContainer>
+ <ThemeProvider>
+<MainApp />
+</ThemeProvider>
  </AuthProvider>
  </Provider>
+ 
 );
 }
+
+const styles = StyleSheet.create({
+container: {
+flex: 1,
+paddingTop: 40,
+},
+light: {
+backgroundColor: "#ffffff",
+},
+dark: {
+backgroundColor: "#121212",
+},
+});
